ea2fc36206b528219f6d54a115d0280496fa4039
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-02-14 17:14:33 +0700

    Исправить дз 1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,6 +1,5 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 
-import pandas as pd
 import psycopg2
 
 # Параметры для подключения к базе данных
@@ -12,6 +11,15 @@ params_bd = {
 }
 
 
+# Функция для чтения файлов .sql
+def read_file_sql(file_sql):
+    try:
+        with open(file_sql) as f:
+            return [line.strip() for line in f]
+    except Exception as er:
+        print(f"Error: {er}")
+
+
 # Функция для SQL запросов
 def execute_query(param):
     # Подключение к базе данных
@@ -20,14 +28,14 @@ def execute_query(param):
 
     try:
         # Чтение файлов csv.
-        employees_data = pd.read_csv("north_data/employees_data.csv")
-        customers_data = pd.read_csv("north_data/customers_data.csv")
-        orders_data = pd.read_csv("north_data/orders_data.csv")
+        employees_data = read_file_sql("north_data/employees_data.csv")
+        customers_data = read_file_sql("north_data/customers_data.csv")
+        orders_data = read_file_sql("north_data/orders_data.csv")
 
         # Передача файлов в таблицы
-        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employees_data.values)
-        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", customers_data.values)
-        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", orders_data.values)
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employees_data)
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", customers_data)
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", orders_data)
 
         # Перадача в базу данных
         conn.commit()
